7ca3fa6e1063f89d934465cfd559b48413ae8040
Kyle Stetz <kyle@punkave.com>
2014-08-02 18:18:28 -0400

    make it synchronous

--- public/js/loadSockets.js
+++ public/js/loadSockets.js
@@ -1,4 +1,13 @@
-$.getScript('/socket.io/socket.io.js', function() {
-  apos.socket = io.connect(window.location.host);
-  apos.emit('socketReady');
+$.ajax({
+    async:false,
+    type:'GET',
+    url: '/socket.io/socket.io.js',
+    data:null,
+    dataType:'script',
+    error: function(xhr, textStatus, errorThrown) {
+      console.error('apostrophe-websockets failed to load socket.io asset.');
+    },
+    success: function() {
+      apos.socket = io.connect(window.location.host);
+    }
 });
